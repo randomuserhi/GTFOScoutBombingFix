b240183861dd2ac57074f44855148ba279827ee5
Christopher Teo <40913834+randomuserhi@users.noreply.github.com>
2024-05-28 23:44:14 +0100

    chore: initial

---
+++ ScoutBombingFix/ScoutBombingFix/Patches/Fix.cs
@@ -0,0 +1,16 @@
+﻿using Enemies;
+using HarmonyLib;
+
+namespace BioScannerFix {
+    [HarmonyPatch]
+    internal static class Fix {
+        [HarmonyPatch(typeof(EnemyDetection), nameof(EnemyDetection.UpdateHibernationDetection))]
+        [HarmonyPrefix]
+        private static void Prefix_UpdateHibernationDetection(EnemyDetection __instance) {
+            if (__instance.m_ai.Agent.CourseNode.m_playerCoverage.GetNodeDistanceToClosestPlayer_Unblocked() > 2) {
+                __instance.m_ai.m_behaviourData.Targets.Clear();
+                __instance.m_ai.m_behaviourData.m_targetsByID.Clear();
+            }
+        }
+    }
+}
